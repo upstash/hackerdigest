56cefd4402f4dba0d1700f40c6e612f1d0e9e872
ogzhanolguncu <ogzhan11@gmail.com>
2023-11-06 12:57:26 +0300

    Remove getArticles from api

--- src/app/api/summarize/route.ts
+++ src/app/api/summarize/route.ts
@@ -1,14 +1,11 @@
 import { NextResponse } from "next/server"
 import { verifySignatureEdge } from "@upstash/qstash/dist/nextjs"
 
-import { getArticles } from "@/app/commands/get"
 import { setArticles } from "@/app/commands/set"
 import { getSummarizedArticles } from "@/app/services/summarizer"
 
 async function handler() {
   console.log("Starting to summarize data from HackerNews")
-  const articlesFromCache = await getArticles()
-  if (articlesFromCache) return NextResponse.json({ articlesFromCache })
 
   const articles = await getSummarizedArticles(8)
   await setArticles(articles)
