fd9c46bf90937e230e7b2c638440268aad06e563
ogzhanolguncu <ogzhan11@gmail.com>
2023-11-06 22:25:28 +0300

    Receive with qstash client v2

--- src/app/api/summarize/route.ts
+++ src/app/api/summarize/route.ts
@@ -16,6 +16,7 @@ async function handler(req: NextRequest) {
 
   // @ts-ignore This can throw errors during vercel build
   const signature = req.headers.get("upstash-signature")
+  console.log({ signature })
   if (!signature) {
     return new NextResponse(new TextEncoder().encode("`Upstash-Signature` header is missing"), {
       status: 403,
@@ -30,6 +31,7 @@ async function handler(req: NextRequest) {
     signature,
     body,
   })
+  console.log({ isValid })
   if (!isValid) {
     return new NextResponse(new TextEncoder().encode("invalid signature"), { status: 403 })
   }
