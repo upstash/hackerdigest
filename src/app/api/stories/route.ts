743f80b8ae274d5d1a60d33665329b0808916b60
ogzhanolguncu <ogzhan11@gmail.com>
2023-11-06 22:20:44 +0300

    Receive with qstash client

--- src/app/api/stories/route.ts
+++ src/app/api/stories/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(req: NextRequest) {
-  return NextResponse.json({ ip: req.ip })
+  const ip = (req.headers.get("x-forwarded-for") ?? "127.0.0.1").split(",")[0]
+
+  return NextResponse.json({ ip })
 }
--- src/app/api/summarize/route.ts
+++ src/app/api/summarize/route.ts
@@ -1,13 +1,38 @@
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 import { setArticles } from "@/commands/set"
 import { getSummarizedArticles } from "@/services/summarizer"
-import { verifySignatureEdge } from "@upstash/qstash/dist/nextjs"
+import { Receiver } from "@upstash/qstash/."
 
 export const maxDuration = 299
 export const dynamic = "force-dynamic"
 
-async function handler() {
+async function handler(req: NextRequest) {
   console.log("Starting to summarize data from HackerNews")
+
+  const receiver = new Receiver({
+    currentSigningKey: process.env.QSTASH_CURRENT_SIGNING_KEY!,
+    nextSigningKey: process.env.QSTASH_NEXT_SIGNING_KEY!,
+  })
+
+  // @ts-ignore This can throw errors during vercel build
+  const signature = req.headers.get("upstash-signature")
+  if (!signature) {
+    return new NextResponse(new TextEncoder().encode("`Upstash-Signature` header is missing"), {
+      status: 403,
+    })
+  }
+  if (typeof signature !== "string") {
+    throw new Error("`Upstash-Signature` header is not a string")
+  }
+
+  const body = await req.text()
+  const isValid = await receiver.verify({
+    signature,
+    body,
+  })
+  if (!isValid) {
+    return new NextResponse(new TextEncoder().encode("invalid signature"), { status: 403 })
+  }
 
   const articles = await getSummarizedArticles(10)
   if (articles) {
@@ -19,4 +44,4 @@ async function handler() {
   }
 }
 
-export const POST = verifySignatureEdge(handler)
+export const POST = handler
