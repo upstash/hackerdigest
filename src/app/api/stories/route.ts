1ea22e38882153035218cee62ba73fcd33688e8b
ogzhanolguncu <ogzhan11@gmail.com>
2023-11-06 23:47:28 +0300

    Add api to fetch summarized articles with ratelimiting

--- src/app/api/stories/route.ts
+++ src/app/api/stories/route.ts
@@ -1,7 +1,22 @@
 import { NextRequest, NextResponse } from "next/server"
+import { Ratelimit } from "@upstash/ratelimit" // for deno: see above
+import { Redis } from "@upstash/redis"
+
+import { getAllSummarizedArticles } from "@/app/actions/get-all-summarized-articles"
 
 export async function GET(req: NextRequest) {
-  const ip = (req.headers.get("x-forwarded-for") ?? "127.0.0.1").split(",")[0]
+  const ratelimit = new Ratelimit({
+    redis: Redis.fromEnv(),
+    limiter: Ratelimit.slidingWindow(10, "10s"),
+    prefix: "hackerdigest-stories",
+  })
 
-  return NextResponse.json({ ip })
+  const ip = (req.headers.get("x-forwarded-for") ?? "127.0.0.1").split(",")[0]
+  const { success } = await ratelimit.limit(ip)
+  if (!success) {
+    return NextResponse.json({ message: "You shall not pass!" })
+  }
+  const { stories } = await getAllSummarizedArticles()
+
+  return NextResponse.json({ stories })
 }
