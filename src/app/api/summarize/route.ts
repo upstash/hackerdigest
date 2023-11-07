a6b96590bb8f7f672d1b42a2ee12132b58ce5086
ogzhanolguncu <ogzhan11@gmail.com>
2023-11-07 17:31:49 +0300

    Remove logs

--- src/app/api/summarize/route.ts
+++ src/app/api/summarize/route.ts
@@ -3,41 +3,16 @@ import { setArticles } from "@/commands/set"
 import { getSummarizedArticles } from "@/services/summarizer"
 
 async function handler() {
-  //   console.log("starting")
-  //   const receiver = new Receiver({
-  //     currentSigningKey: process.env.QSTASH_CURRENT_SIGNING_KEY!,
-  //     nextSigningKey: process.env.QSTASH_NEXT_SIGNING_KEY!,
-  //   })
-
-  //   const signature = req.headers.get("Upstash-Signature")
-  //   if (!signature) {
-  //     return new NextResponse(new TextEncoder().encode("`Upstash-Signature` header is missing"), {
-  //       status: 403,
-  //     })
-  //   }
-  //   if (typeof signature !== "string") {
-  //     throw new Error("`Upstash-Signature` header is not a string")
-  //   }
-  //   const body = await req.text()
-  //   await receiver.verify({
-  //     signature,
-  //     body,
-  //   })
-
   try {
-    const articles = await getSummarizedArticles(1)
+    const articles = await getSummarizedArticles(10)
 
     if (articles) {
-      console.log("articles", articles)
       await setArticles(articles)
-      console.log("returning articles")
       return NextResponse.json({ articles })
     } else {
-      console.error("Something went wrong articles are missing!")
-      return NextResponse.json({ message: "asdasdas" }, { status: 400 })
+      return NextResponse.json({}, { status: 400 })
     }
   } catch (error) {
-    console.log("error", (error as Error).message)
     return NextResponse.json({}, { status: 400 })
   }
 }
