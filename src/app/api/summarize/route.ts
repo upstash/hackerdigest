15f163294d5191e6fc1c73390f5dbc05d6b7f8ed
ogzhanolguncu <ogzhan11@gmail.com>
2023-11-16 11:50:26 +0300

    Add qstash back to api

--- src/app/api/summarize/route.ts
+++ src/app/api/summarize/route.ts
@@ -1,25 +1,26 @@
-// import { NextResponse } from "next/server"
-// import { setArticles } from "@/commands/set"
-// import { getSummarizedArticles } from "@/services/summarizer"
-
 import { NextResponse } from "next/server"
-import { fetchTopStoriesFromLast12Hours } from "@/services/hackernews"
+import { setArticles } from "@/commands/set"
+import { getSummarizedArticles } from "@/services/summarizer"
+import { verifySignatureAppRouter } from "@upstash/qstash/dist/nextjs"
 
 export const maxDuration = 300
 
 async function handler() {
-  return NextResponse.json(await fetchTopStoriesFromLast12Hours())
-  //   try {
-  //     const articles = await getSummarizedArticles(10)
-  //     if (articles) {
-  //       await setArticles(articles)
-  //       return NextResponse.json({ articles })
-  //     } else {
-  //       return NextResponse.json({}, { status: 400 })
-  //     }
-  //   } catch (error) {
-  //     return NextResponse.json({}, { status: 400 })
-  //   }
+  try {
+    const articles = await getSummarizedArticles(10)
+    if (articles) {
+      await setArticles(articles)
+      return NextResponse.json({ articles })
+    } else {
+      console.error("Stories are missing!")
+      return NextResponse.json({}, { status: 400 })
+    }
+  } catch (error) {
+    console.error(
+      `Something went wrong when saving to cache or retriving stories ${(error as Error).message}`
+    )
+    return NextResponse.json({}, { status: 400 })
+  }
 }
 
-export const GET = handler
+export const POST = verifySignatureAppRouter(handler)
