f9bf90ff5f1b184baaf1995096a95dbb243d3885
fahreddinozcan <ozcanfahrettinn@gmail.com>
2024-02-01 17:18:48 +0300

    enable analytics

--- src/app/api/stories/route.ts
+++ src/app/api/stories/route.ts
@@ -1,22 +1,23 @@
-import { NextRequest, NextResponse } from "next/server"
-import { Ratelimit } from "@upstash/ratelimit"
-import { Redis } from "@upstash/redis"
+import { NextRequest, NextResponse } from "next/server";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
 
-import { getAllSummarizedArticles } from "@/app/actions/get-all-summarized-articles"
+import { getAllSummarizedArticles } from "@/app/actions/get-all-summarized-articles";
 
 export async function GET(req: NextRequest) {
   const ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
     limiter: Ratelimit.slidingWindow(10, "10s"),
     prefix: "hackerdigest-stories",
-  })
+    analytics: true,
+  });
 
-  const ip = (req.headers.get("x-forwarded-for") ?? "127.0.0.1").split(",")[0]
-  const { success } = await ratelimit.limit(ip)
+  const ip = (req.headers.get("x-forwarded-for") ?? "127.0.0.1").split(",")[0];
+  const { success } = await ratelimit.limit(ip);
   if (!success) {
-    return NextResponse.json({ message: "You shall not pass!" })
+    return NextResponse.json({ message: "You shall not pass!" });
   }
-  const { stories } = await getAllSummarizedArticles()
+  const { stories } = await getAllSummarizedArticles();
 
-  return NextResponse.json({ stories })
+  return NextResponse.json({ stories });
 }
